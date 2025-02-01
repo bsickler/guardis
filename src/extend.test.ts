8336b9dcadb62b91184f6324eb8a02221571125a
Burton Sickler <100096377+MrPossumz@users.noreply.github.com>
2025-01-31 18:08:40 -0800

    extends extends

--- src/extend.test.ts
+++ src/extend.test.ts
@@ -1,23 +1,51 @@
 import { assert, assertFalse, assertThrows } from "@std/assert";
 import { extend } from "./extend.ts";
 
-Deno.test("extend", () => {
-  const Is = extend({
-    MySillyType: (v: unknown) => {
-      if (v === 10 || v === "meatball") {
-        return v;
-      }
+Deno.test("extend", async (t) => {
+  await t.step("basic", () => {
+    const Is = extend({
+      MySillyType: (v: unknown) => {
+        if (v === 10 || v === "meatball") {
+          return v;
+        }
 
-      return null;
-    },
+        return null;
+      },
+    });
+
+    assert("MySillyType" in Is);
+    assert(Is.MySillyType(10));
+    assert(Is.MySillyType("meatball"));
+    assertFalse(Is.MySillyType(1));
+
+    assert("strict" in Is.MySillyType);
+    assert(Is.MySillyType.strict(10));
+    assertThrows(() => Is.MySillyType.strict("sausage"));
   });
 
-  assert("MySillyType" in Is);
-  assert(Is.MySillyType(10));
-  assert(Is.MySillyType("meatball"));
-  assertFalse(Is.MySillyType(1));
+  await t.step("iteration", () => {
+    const Is = extend({
+      Meatball: (v: unknown) => v === "meatball" ? v : null,
+    });
 
-  assert("strict" in Is.MySillyType);
-  assert(Is.MySillyType.strict(10));
-  assertThrows(() => Is.MySillyType.strict("sausage"));
+    assert("Meatball" in Is);
+    assert(Is.Meatball("meatball"));
+    assertFalse(Is.Meatball(1));
+
+    assertFalse("Sauage" in Is);
+
+    const IsTwo = extend(Is, {
+      Sausage: (v: unknown) => v === "sausage" ? v : null,
+    });
+
+    assert("Sausage" in IsTwo);
+    assert(IsTwo.Sausage("sausage"));
+    assertFalse(IsTwo.Sausage(1));
+
+    assert("Meatball" in IsTwo);
+    assert(IsTwo.Meatball("meatball"));
+    assertFalse(IsTwo.Meatball(1));
+		
+    assertFalse("Sauage" in Is);
+  });
 });
