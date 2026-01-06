cbe52aafc0c08437455f8cc7950e16ebb736c0a7
bsickler <bsickler@spudlabs.com>
2026-01-06 11:43:17 -0800

    adds transformation function support to guardis-hono

--- hono/mod.ts
+++ hono/mod.ts
@@ -21,7 +21,7 @@ type ValidationTargetByMethod<M> = M extends "get" | "head"
  * @template U - The validation target, which is a key in `ValidationTargetByMethod<M>`.
  *
  * @param target - The validation target, which determines the type of validation to perform.
- * @param fn - A type guard function that validates the input and returns a boolean.
+ * @param validationFn - A type guard function that validates the input and returns a boolean.
  *
  * @returns A validator function that validates the input and either returns the validated value
  *          or responds with an error message and a 400 status code if validation fails.
@@ -37,16 +37,18 @@ type ValidationTargetByMethod<M> = M extends "get" | "head"
  * ```
  */
 export const describeInput = <
-  OutputType,
+  ValidationType,
+  OutputType = ValidationType,
   M extends string = string,
   // deno-lint-ignore no-explicit-any
-  U extends ValidationTargetByMethod<M> = any,
+  T extends ValidationTargetByMethod<M> = any,
 >(
-  target: U,
-  fn: TypeGuard<OutputType>,
+  target: T,
+  validationFn: TypeGuard<ValidationType>,
+  transformFn?: (input: ValidationType) => OutputType,
 ) => {
   return validator(target, (value, c): OutputType | Response => {
-    if (fn(value)) return value;
+    if (validationFn(value)) return transformFn ? transformFn(value) : value;
 
     return c.json({ message: `Input validation failed for target: ${target}` }, 400);
   });
